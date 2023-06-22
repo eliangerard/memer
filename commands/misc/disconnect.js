9a3ca65fd652e3948100986883ccb103c791f0b8
Elian <eliangerardiso@gmail.com>
2023-06-21 22:00:40 -0600

    Cleaned misc commands

--- commands/misc/disconnect.js
+++ commands/misc/disconnect.js
@@ -1,31 +1,14 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { EmbedBuilder } = require("discord.js");
 
-const leave = async (channel, client) => {
-	const voiceJoined = await client.distube.voices.get(channel);
-	voiceJoined.leave();
-	const embed = new EmbedBuilder()
-		.setTitle(client.emotes.success + " Adiós")
-		.setColor("#efefef");
-	return embed;
-}
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('disconnect')
-		.setDescription('Desconecta el bot del canal de voz'),
+	.setName('disconnect')
+	.setDescription('Desconecta el bot del canal de voz'),
 	inVoice: true,
 	voiceCommand: ['salte', 'desconéctate'],
-	async executeInteraction(interaction, client) {
-		const embed = await leave(interaction.member.voice.channel, client)
-		interaction.editReply({ embeds: [embed] }).then(msg => {
-			setTimeout(() => msg.delete(), 15000)
-		});
-	},
-	async execute(content, msg, client) {
-		const embed = await leave(msg.member.voice.channel, client)
-		client.channel.send({ embeds: [embed] }).then(msg => {
-			setTimeout(() => msg.delete(), 15000)
-		});
-
+	async execute(client, queue, message, content) {
+		const voiceJoined = await client.distube.voices.get(message.member.voice.channel);
+		voiceJoined.leave();
+		return { title: client.emotes.success + " Adiós" }
 	},
 };
--- commands/misc/help.js
+++ commands/misc/help.js
@@ -1,69 +1,4 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { EmbedBuilder } = require("discord.js");
-
-const help = (commandResolvable = null, client) => {
-    if (commandResolvable) {
-        let command = client.commands.get(commandResolvable);
-        if (!command)
-            command = client.commands.find(cmd => {
-                console.log(cmd.alias);
-                return cmd.alias && cmd.alias.includes(commandResolvable)
-            });
-
-        if (!command) {
-            const embed = new EmbedBuilder()
-                .setTitle('Comando no encontrado')
-                .setColor('#0099ff')
-                .setTimestamp()
-                .setFooter({ text: 'Memer', iconURL: client.botURL });
-            return embed;
-        }
-        console.log(command);
-        const fields = [
-            {
-                name: ' ',
-                value: command.data.description
-            }
-        ];
-
-        if (command.alias)
-            fields.push({
-                name: 'Alias para texto',
-                value: '`' + (command.alias ? command.alias.join(', ') : 'N/A') + '`'
-            });
-
-        if (command.voiceCommand)
-            fields.push({
-                name: 'Comando por voz',
-                value: '`' + command.voiceCommand.join(', ') + '`'
-            })
-
-        const embed = new EmbedBuilder()
-            .setTitle(client.config.prefix + command.data.name)
-            .setColor('#0099ff')
-            .addFields(fields)
-            .setTimestamp()
-            .setFooter({ text: 'Memer', iconURL: client.botURL });
-        return embed;
-    }
-    const fields = client.commands.map(command => ({
-        name: ' ',
-        value: '`' + client.config.prefix + command.data.name + '` - ' + command.data.description
-    }));
-
-    fields.push({
-        name: 'Por comando',
-        value: 'Usa `' + client.config.prefix + 'help <comando>` para ver sus alias y su activación por voz'
-    })
-    const embed = new EmbedBuilder()
-        .setTitle('Comandos disponibles')
-        .setColor('#0099ff')
-        .addFields(fields)
-        .setTimestamp()
-        .setFooter({ text: 'Memer', iconURL: client.botURL });
-
-    return embed;
-}
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -73,13 +8,52 @@ module.exports = {
     inVoice: false,
     alias: ['h'],
     voiceCommand: ['ayuda', 'comandos'],
-    async executeInteraction(interaction, client) {
-        const commandResolvable = interaction.options.getString('comando');
-        const embed = help(commandResolvable, client);
-        interaction.editReply({ embeds: [embed] });
-    },
-    async execute(content, message, client) {
-        const embed = help(content, client);
-        message.reply({ embeds: [embed] });
+    async execute(client, queue, message, content) {
+        const [commandResolvable] = content;
+        if (commandResolvable) {
+            let command = client.commands.get(commandResolvable);
+            if (!command)
+                command = client.commands.find(cmd => cmd.alias && cmd.alias.includes(commandResolvable));
+            if (!command)
+                return { title: 'Comando no encontrado' };
+
+            const fields = [
+                {
+                    name: ' ',
+                    value: command.data.description
+                }
+            ];
+
+            if (command.alias)
+                fields.push({
+                    name: 'Alias para texto',
+                    value: '`' + (command.alias ? command.alias.join(', ') : 'N/A') + '`'
+                });
+
+            if (command.voiceCommand)
+                fields.push({
+                    name: 'Comando por voz',
+                    value: '`' + command.voiceCommand.join(', ') + '`'
+                })
+
+            return {
+                title: client.config.prefix + command.data.name,
+                fields
+            };
+        }
+        const fields = client.commands.map(command => ({
+            name: ' ',
+            value: '`' + client.config.prefix + command.data.name + '` - ' + command.data.description
+        }));
+
+        fields.push({
+            name: 'Por comando',
+            value: 'Usa `' + client.config.prefix + 'help <comando>` para ver sus alias y su activación por voz'
+        })
+
+        return {
+            title: 'Comandos disponibles',
+            fields
+        };
     },
 };
