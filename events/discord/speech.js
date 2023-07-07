d76c4b2ed9d03995465477abe6994ffe2831d8e8
Elian <eliangerardiso@gmail.com>
2023-07-06 20:52:30 -0600

    Added button extended support to voice commands

--- events/discord/speech.js
+++ events/discord/speech.js
@@ -33,7 +33,17 @@ module.exports = {
 				});
 			}
 
-			const { title = null, description = null, fields = [], image = null, thumbnail = null, react = [], handler = null } = await command.execute(client, queue, message, params);
+			const {
+                title = null,
+                description = null,
+                fields = [],
+                image = null,
+                thumbnail = null,
+                react = [],
+                handler = null,
+                actionRows = null,
+                resetTimeout = false
+            } = await command.execute(client, queue, message, params);
 
 			const embed = new EmbedBuilder()
 				.setTitle(title)
@@ -45,12 +55,22 @@ module.exports = {
 				.setTimestamp()
 				.setFooter({ text: client.user.username, iconURL: client.botURL });
 
-			await message.channel.send({ embeds: [embed] }).then(msg => {
+			await message.channel.send({ embeds: [embed], components: [actionRows] }).then(msg => {
 				if (react.length > 0) {
-					react.forEach(emoji => msg.react(emoji))
-					client.on('messageReactionAdd', handler(reaction, user, msg));
+                    const newHandler = (reaction, user) => handler(reaction, user, msg, queue, client, newHandler);
+                    react.forEach(emoji => msg.react(emoji))
+                    client.on('messageReactionAdd', newHandler);
+                }
+				else {
+					if( resetTimeout ) {
+						const timeout = setTimeout(() => msg.delete(), 20000);
+						client.timeouts[msg.id] = {
+							timeout,
+							msg
+						};
+					}
+					setTimeout(() => msg.delete(), 15000);
 				}
-				else setTimeout(() => msg.delete(), 15000)
 			});
 		} catch (error) {
 			console.error(error);
